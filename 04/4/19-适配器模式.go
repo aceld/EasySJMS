3432e6d37110ca55e7316d7a0154e4d90be43e49
Aceld <liudanbing@tal.com>
2022-07-04 19:32:13 +0800

    适配器模式

---
+++ 04/4/19-适配器模式.go
@@ -0,0 +1,56 @@
+package main
+
+import "fmt"
+
+//适配的目标
+type V5 interface {
+	Use5V()
+}
+
+//业务类，依赖V5接口
+type Phone struct {
+	v V5
+}
+
+func NewPhone(v V5) *Phone {
+	return &Phone{v}
+}
+
+func (p *Phone) Charge() {
+	fmt.Println("Phone进行充电...")
+	p.v.Use5V()
+}
+
+
+//被适配的角色，适配者
+type V220 struct {}
+
+func (v *V220) Use220V() {
+	fmt.Println("使用220V的电压")
+}
+
+//电源适配器
+type Adapter struct {
+	v220 *V220
+}
+
+func (a *Adapter) Use5V() {
+	fmt.Println("使用适配器进行充电")
+
+	//调用适配者的方法
+	a.v220.Use220V()
+}
+
+func NewAdapter(v220 *V220) *Adapter {
+	return &Adapter{v220}
+}
+
+
+
+// ------- 业务逻辑层 -------
+func main() {
+	iphone := NewPhone(NewAdapter(new(V220)))
+
+	iphone.Charge()
+}
+
