aa1969bacd1ef2e69d0d8d203a4e2b249aaf48f9
Aceld <liudanbing@tal.com>
2022-07-07 18:11:10 +0800

    观察者模式

---
+++ 05/8/28-观察者模式.go
@@ -0,0 +1,109 @@
+package main
+
+import "fmt"
+
+//--------- 抽象层 --------
+
+//抽象的观察者
+type Listener interface {
+	OnTeacherComming()	 //观察者得到通知后要触发的动作
+}
+
+type Notifier interface {
+	AddListener(listener Listener)
+	RemoveListener(listener Listener)
+	Notify()
+}
+
+//--------- 实现层 --------
+//观察者学生
+type StuZhang3 struct {
+	Badthing string
+}
+
+func (s *StuZhang3) OnTeacherComming() {
+	fmt.Println("张3 停止 ", s.Badthing)
+}
+
+func (s *StuZhang3) DoBadthing() {
+	fmt.Println("张3 正在", s.Badthing)
+}
+
+type StuZhao4 struct {
+	Badthing string
+}
+
+func (s *StuZhao4) OnTeacherComming() {
+	fmt.Println("赵4 停止 ", s.Badthing)
+}
+
+func (s *StuZhao4) DoBadthing() {
+	fmt.Println("赵4 正在", s.Badthing)
+}
+
+type StuWang5 struct {
+	Badthing string
+}
+
+func (s *StuWang5) OnTeacherComming() {
+	fmt.Println("王5 停止 ", s.Badthing)
+}
+
+func (s *StuWang5) DoBadthing() {
+	fmt.Println("王5 正在", s.Badthing)
+}
+
+
+//通知者班长
+type ClassMonitor struct {
+	listenerList []Listener //需要通知的全部观察者集合
+}
+
+func (m *ClassMonitor) AddListener(listener Listener) {
+	m.listenerList = append(m.listenerList, listener)
+}
+
+func (m *ClassMonitor) RemoveListener(listener Listener) {
+	for index, l := range m.listenerList {
+		//找到要删除的元素位置
+		if listener == l {
+			//将删除的点前后的元素链接起来
+			m.listenerList = append(m.listenerList[:index], m.listenerList[index+1:]...)
+			break
+		}
+	}
+}
+
+func (m* ClassMonitor) Notify() {
+	for _, listener := range m.listenerList {
+		//依次调用全部观察的具体动作
+		listener.OnTeacherComming()
+	}
+}
+
+
+func main() {
+	s1 := &StuZhang3{
+		Badthing: "抄作业",
+	}
+	s2 := &StuZhao4{
+		Badthing: "玩王者荣耀",
+	}
+	s3 := &StuWang5{
+		Badthing: "看赵四玩王者荣耀",
+	}
+
+	classMonitor := new(ClassMonitor)
+
+	fmt.Println("上课了，但是老师没有来，学生们都在忙自己的事...")
+	s1.DoBadthing()
+	s2.DoBadthing()
+	s3.DoBadthing()
+
+	classMonitor.AddListener(s1)
+	classMonitor.AddListener(s2)
+	classMonitor.AddListener(s3)
+
+	fmt.Println("这时候老师来了，班长给学什么使了一个眼神...")
+	classMonitor.Notify()
+}
