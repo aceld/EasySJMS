5ffe20076d6cebb579dd2df91b872c4fa2ba486e
shaouai <56815047+dushaoshuai@users.noreply.github.com>
2023-03-04 18:31:09 +0800

    no format

--- 03/7/13-线程安全的单例模式-1.go
+++ 03/7/13-线程安全的单例模式-1.go
@@ -5,15 +5,15 @@ import (
 	"sync"
 )
 
-// 定义锁
+//定义锁
 var lock sync.Mutex
 
-type singelton struct{}
+type singelton struct {}
 
 var instance *singelton
 
 func GetInstance() *singelton {
-	// 为了线程安全，增加互斥
+	//为了线程安全，增加互斥
 	lock.Lock()
 	defer lock.Unlock()
 
@@ -26,6 +26,7 @@ func GetInstance() *singelton {
 func (s *singelton) SomeThing() {
 	fmt.Println("单例对象的某方法")
 }
+
 
 func main() {
 	s := GetInstance()
