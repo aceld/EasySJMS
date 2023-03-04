5cf9c15004f39a935e5dc398abd01aa69d667854
shaouai <1528606126@qq.com>
2023-03-04 18:16:35 +0800

    fix singleton initialization error

--- 03/6/12-单例模式懒汉式.go
+++ 03/6/12-单例模式懒汉式.go
@@ -2,18 +2,17 @@ package main
 
 import "fmt"
 
-type singelton struct {}
+type singelton struct{}
 
 var instance *singelton
 
 func GetInstance() *singelton {
-	//只有首次GetInstance()方法被调用，才会生成这个单例的实例
+	// 只有首次GetInstance()方法被调用，才会生成这个单例的实例
 	if instance == nil {
 		instance = new(singelton)
-		return instance
 	}
 
-	//接下来的GetInstance直接返回已经申请的实例即可
+	// 接下来的GetInstance直接返回已经申请的实例即可
 	return instance
 }
 
--- 03/7/13-线程安全的单例模式-1.go
+++ 03/7/13-线程安全的单例模式-1.go
@@ -5,29 +5,27 @@ import (
 	"sync"
 )
 
-//定义锁
+// 定义锁
 var lock sync.Mutex
 
-type singelton struct {}
+type singelton struct{}
 
 var instance *singelton
 
 func GetInstance() *singelton {
-	//为了线程安全，增加互斥
+	// 为了线程安全，增加互斥
 	lock.Lock()
 	defer lock.Unlock()
 
 	if instance == nil {
-		return new(singelton)
-	} else {
-		return instance
+		instance = new(singelton)
 	}
+	return instance
 }
 
 func (s *singelton) SomeThing() {
 	fmt.Println("单例对象的某方法")
 }
-
 
 func main() {
 	s := GetInstance()
